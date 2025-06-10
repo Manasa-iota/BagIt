c1f4c3d9b889092b28bd9aa40012deddc6674a42
Manasa-iota <nagarammanasa235@gmail.com>
2025-06-10 10:22:20 +0530

    add item list

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,5 +1,24 @@
+import { items } from "../lib/constants";
+
 export default function ItemList() {
   return (
-    <div className="item-list">ItemList</div>
-  )
+    <ul className="item-list">
+      {items.map((item) => (
+        <Item key={item.id} item={item} />
+      ))}
+    </ul>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <>
+      <li className="item">
+        <label>
+          <input type="checkbox" checked={item.packed} />
+          {item.name}
+        </label>
+      </li>
+    </>
+  );
 }
