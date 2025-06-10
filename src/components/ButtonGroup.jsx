6a53d91dc0c1e00810f4650daa677fb952faf0a1
Manasa-iota <nagarammanasa235@gmail.com>
2025-06-10 09:59:08 +0530

    move static array outside the component

--- src/components/ButtonGroup.jsx
+++ src/components/ButtonGroup.jsx
@@ -1,7 +1,6 @@
 import Button from "./Button";
-
+const secondaryButtons = ["Mark All as complete", "Mark All as incomplete", "Reset to Initial", "Remove all items"];
 export default function ButtonGroup() {
-    const secondaryButtons = ["Mark All as complete", "Mark All as incomplete", "Reset to Initial", "Remove all items"];
   return (
     <section className="button-group">
         {secondaryButtons.map(((text,index )=> <Button key={index} type="secondary" text={text} />
